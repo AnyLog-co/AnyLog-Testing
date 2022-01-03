eaa145182663761355a952cae4241a3618fa9fdb
Oi Shadmon <oshadmon@gmail.com>
2022-01-03 12:29:26 -0800

    pytest prep moved

--- tests/pytest_prep.py
+++ tests/pytest_prep.py
@@ -1,7 +1,11 @@
 import os
 import pytest
 import shutil
+import sys
 
+ROOT_DIR = os.path.expandvars(os.path.expanduser(__file__)).split('tests')[0]
+SUPPORT_DIR = os.path.join(ROOT_DIR, 'support')
+sys.path.insert(0, SUPPORT_DIR)
 import file_io
 import rest_get
 import send_data
@@ -28,13 +32,13 @@ def setup_code(table_name:list, config_file:str, data_dir:str, expected_dir:str,
     :return:
         status, configs
     """
-    if not os.path.isdir(expected_dir):
-        pytest.fail('Failed to locate expected results dir')
-    if not os.path.isdir(actual_dir):
-        try:
-            os.makedirs(actual_dir)
-        except Expection as e:
-            pytes.fail('Failed to created directory: %s (Error: %s)' % (actual_dir, e))
+    # if not os.path.isdir(expected_dir):
+    #     pytest.fail('Failed to locate expected results dir')
+    # if not os.path.isdir(actual_dir):
+    #     try:
+    #         os.makedirs(actual_dir)
+    #     except Expection as e:
+    #         pytes.fail('Failed to created directory: %s (Error: %s)' % (actual_dir, e))
 
     configs = file_io.read_configs(config_file=config_file)
     status = rest_get.get_status(conn=configs['conn'], username=configs['rest_user'],
@@ -57,3 +61,4 @@ def teardown_code(actual_dir:str):
             shutil.rmtree(actual_dir)
         except Exception as e:
             pytest.fail('Failed to drop actual results directory: %s (Error: %s)' % (actual_dir, e))
+
